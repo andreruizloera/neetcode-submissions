bbb914e61ad6eec5e8da3c001ead9997b5730384
Andre <andre.x.ruizloera@gmail.com>
2026-08-31 20:36:27 -0500

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+       unordered_set<int> seen;   // create an empty set
+
+        for (int i = 0; i < nums.size(); i++) {
+            int value = nums[i];  // the current number in the array
+
+            // check if we've seen this number before
+            if (seen.count(value)) {
+                return true;      // duplicate found
+            }
+
+            // otherwise insert it into the set
+            seen.insert(value);
+        }
+
+        return false;             // no duplicates in the entire vector
+    } 
+};
---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,18 @@
+using namespace std; 
+#include <vector> 
+#include <unordered_set>
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+    unordered_set<int> seen; 
+
+    for(int n : nums) { 
+        if(seen.count(n)) {
+            return true; 
+        }
+        seen.insert(n); 
+    }
+    return false;
+}
+};
